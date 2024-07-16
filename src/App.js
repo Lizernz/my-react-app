63a5e57f9661be72ff5495c717f13b3dde1bb1ce
Lizernz <154061095+Lizernz@users.noreply.github.com>
2024-07-16 22:19:52 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" >
+    <h1>Weather Apps</h1>
+    <button className="btn btn-primary">Hello</button>
+    <footer>
+      This project was coded by Elizabeth Lavender
+      and is <a href="https://github.com/Lizernz/my-react-app"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+         open-sourced on GitHub
+      </a> and on 
+      <a href="https://lizernz-react-app.netlify.app/"
+      target="_blank"
+      rel="noopener noreferrer"
+      >
+        Netlify
+      </a>
+    </footer>
+    </div>
     </div>
   );
 }
