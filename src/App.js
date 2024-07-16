98b8e1abaf8fb1c2babff79cd031fae10e739a93
Lizernz <154061095+Lizernz@users.noreply.github.com>
2024-07-16 23:01:14 +0100

    Drafted the HTML page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <div className="container" >
-    <h1>Weather Apps</h1>
-    <button className="btn btn-primary">Hello</button>
+    <h1>Weather App</h1>
+
+   <Weather />
     <footer>
       This project was coded by Elizabeth Lavender
       and is <a href="https://github.com/Lizernz/my-react-app"
